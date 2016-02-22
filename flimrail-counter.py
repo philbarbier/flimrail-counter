37ad5e05bfdea272f001994134149a2843ea17cc
Phil B <philb@seepies.net>
2016-02-22 02:44:53 -0500

    error handling if file doesn\'t exist and some cleanup

--- flimrail-counter.py
+++ flimrail-counter.py
@@ -15,46 +15,60 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_LISTEN_IP, UDP_LISTEN_PORT))
 
 def get_counter_value(f) :
-    cv = int(f.read())
-    return cv 
+    cv = f.read()
+    if cv == '' : 
+        return 0
+    return int(cv)
 
 def get_state() :
-    return True 
+    return False 
 
 def main() :
-    
-    with open(CTR_FILE, 'r+') as f:
-        state = 0
-
-        do_read = get_state()
-
-        message = get_counter_value(f)
-        print 'Counter is at: ', message
-        while True:
-            f.seek(0)
-            data, addr = sock.recvfrom(1024)
-
-            state += 1
-
-            if (state % 2) == 0 :
-                do_read = True
+ 
+    try :
+        with open(CTR_FILE, 'r+') as f :
             
-            if do_read :
-                message += 1
+            state = 0
+
+            do_read = get_state()
+
+            message = get_counter_value(f)
+            print 'Counter is at: ', message
+
+            while True:
+                f.seek(0)
+                data, addr = sock.recvfrom(1024)
                 
-                print "RX: ", data
-                print "TX: ", message
-                print "TX-bytes: ", bytes(message)
+                state += 1
+
+                if (state % 2) == 0 :
+                    do_read = True
                 
-                sendsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                print "Send-result: ", sendsock.sendto(bytes(message), (UDP_SEND_IP, UDP_SEND_PORT)) 
-                f.write(str(message))
+                if do_read == True :
+                    message += 1
+                    tx_message = int(str(message)[::-1])
+                    sendsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    f.write(str(message))
+
+                    print "RX: ", data
+                    print "CTR: ", message
+                    print "TX: ", tx_message
+                    print "TX-bytes: ", bytes(tx_message)
+                    print "SR: ", sendsock.sendto(bytes(tx_message), (UDP_SEND_IP, UDP_SEND_PORT)) 
+
                 do_read = False
-    ### clean up, clean up like a good wittle boy!
-    f.close()
+
+        ### clean up, clean up like a good wittle boy!
+        f.close()
+
+    except IOError :
+        open(CTR_FILE, 'w')
+        ### let's try that again, shall we?
+        main()
 
 if __name__ == '__main__' :
     try :
         main()
+        
     except KeyboardInterrupt :
         print "Oooooook bye"
