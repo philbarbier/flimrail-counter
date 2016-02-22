fa126b65f8cd305b20bdc7f3a50aab568e6d1b18
Phil B <philb@seepies.net>
2016-02-22 00:57:41 -0500

    refactoring

--- flimrail-counter.py
+++ flimrail-counter.py
@@ -14,23 +14,47 @@ CTR_FILE = "flimrail1.ctr"
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_LISTEN_IP, UDP_LISTEN_PORT))
 
-message = 0
+def get_counter_value(f) :
+    cv = int(f.read())
+    return cv 
 
-f = open(CTR_FILE, 'r+')
+def get_state() :
+    return True 
 
-message = int(f.read())
-print 'Counter is at: ', message
-while True:
-    f.seek(0)
-    data, addr = sock.recvfrom(1024)
-
-    message += 1
+def main() :
     
-    print "RX: ", data
-    print "TX: ", message
-    print "TX-bytes: ", bytes(message)
-    
-    sendsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print "Send-result: ", sendsock.sendto(bytes(message), (UDP_SEND_IP, UDP_SEND_PORT)) 
-    f.write(str(message))
-    print f.tell()
+    with open(CTR_FILE, 'r+') as f:
+        state = 0
+
+        do_read = get_state()
+
+        message = get_counter_value(f)
+        print 'Counter is at: ', message
+        while True:
+            f.seek(0)
+            data, addr = sock.recvfrom(1024)
+
+            state += 1
+
+            if (state % 2) == 0 :
+                do_read = True
+            
+            if do_read :
+                message += 1
+                
+                print "RX: ", data
+                print "TX: ", message
+                print "TX-bytes: ", bytes(message)
+                
+                sendsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                print "Send-result: ", sendsock.sendto(bytes(message), (UDP_SEND_IP, UDP_SEND_PORT)) 
+                f.write(str(message))
+                do_read = False
+    ### clean up, clean up like a good wittle boy!
+    f.close()
+
+if __name__ == '__main__' :
+    try :
+        main()
+    except KeyboardInterrupt :
+        print "Oooooook bye"
