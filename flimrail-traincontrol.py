d633e260ada98f1bd286c16f3bc31418b0c6cbb1
aster1sk <admin@riboflav.in>
2016-02-23 23:38:00 -0500

    Pretty much a rewrite

--- flimrail-traincontrol.py
+++ flimrail-traincontrol.py
@@ -2,61 +2,116 @@
 
 ### Receives signal, decodes bits sent and sends control direction
 ### to train system
-
-import socket
+from socket import socket, AF_INET, SOCK_DGRAM
 import logging
 from ConfigParser import ConfigParser
 
-config = ConfigParser()
-config.read('config.ini')
-
-UDP_LISTEN_IP = config.get('inet', 'UDP_LISTEN_IP')
-UDP_LISTEN_PORT = config.getint('inet', 'UDP_LISTEN_PORT')
-
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_LISTEN_IP, UDP_LISTEN_PORT))
+"""
+Think of this like a router - a dictionary of available actions
+I'm assuming redstone chips will be sending integers as bytes
+"""
+
+actions = {
+    0 : "noop",
+    1 : "forward",
+    2 : "reverse",
+    3 : "stop",
+}
 
 
-def control_switcher(command) :
-    cs = {
-        '01': control_go,
-        '03': control_reverse,
-    }
-    m = cs.get(command, noop)
-    return m()
+class TrainCtl :
 
 
-# No Operation - used for debugging or something?
-def noop() :
-    logging.debug("Nothing to do")
-    return
+    def __init__(self, *args, **kwargs) :
+        self.port = kwargs.get('port')
+        self.listen_addr = kwargs.get('listen_addr')
+        logging.debug("Listening on %s:%s" % (self.listen_addr, self.port))
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.bind((self.listen_addr, self.port))
+        logging.debug("Binding socket")
+        self.state = False
+        self.rx() # Sets up the socket, dispatches commands
 
 
-def control_go() :
-    logging.debug('Train is now on')
-    return
+    def rx(self) :
+
+        logging.debug("Starting dispatcher")
+        while True :
+            data, addr = self.socket.recvfrom(1024)
+            try :
+                host, port = addr[0], addr[1]
+            except:
+                logging.error("Invalid host / port")
+                return
+            logging.debug("Received : '%s' from %s:%s" % (data, host, port))
+            # Convert and lookup command to execute
+            payload = {
+                "host" : host,
+                "port" : port,
+                "data" : data,
+            }
+            self.dispatch(payload)
+
+    def dispatch(self, payload) :
+        # Convert bytes to ascii integer
+        try :
+            decode = int(payload.get('data').decode("ascii"))
+        except :
+            logging.error("Could not decode bytes to ASCII")
+            return
+
+        # Map decoded ascii integer to action string for eval in actions list
+        action = actions.get(decode)
+
+        if action : # Action was in actions list, try to evaluate it
+            action = "my_%s" % action
+            try : 
+                logging.debug("Attempting to execute '%s' from %s:%s" % (action, payload.get('host'), payload.get('port')))
+                func = getattr(self, action)
+                return func()
+            except :
+                logging.error("Could not evaluate '%s' or method does not exist from %s:%s" % (action, payload.get('host'), payload.get('port')))
+                return
+
+        # No action was matched but log whatever they sent anyway
+        logging.error("'%s' does not map to a valid action from : %s:%s" % (decode, payload.get('host'), payload.get('port')))
 
 
-def control_reverse() :
-    logging.debug('Train is on and in reverse')
-    return
+    # No Operation - used for debugging or something?
+    def my_noop(self, *args, **kwargs) :
+        logging.debug("Nothing to do")
+        return
+
+
+    def my_stop(self, *args, **kwargs) :
+        logging.debug('Train is stopped')
+        self.state = "halt"
+        return
+
+
+    def my_forward(self, *args, **kwargs) :
+        logging.debug('Train is on and moving forward')
+        self.state = "forward"
+        return
+
+    def my_reverse(self, *args, **kwargs) :
+        logging.debug('Train is on and in reverse')
+        self.state = "reverse"
+        return True
 
 
 def main() :
- 
-    while True:
-        try :
-            data, addr = sock.recvfrom(1024)
-            
-            dr = repr(data)
-            dr = dr[3:len(dr)-1]
 
-            ### print "Data: ", dr 
-            print control_switcher(dr)
-        except:
-            logging.debug("Malformed data")
+    config = ConfigParser()
+    config.read('config.ini')
+
+    TrainCtl(
+        port = config.getint('inet', 'UDP_LISTEN_PORT'),
+        listen_addr = config.get('inet', 'UDP_LISTEN_IP')
+    )
+ 
 
 if __name__ == '__main__' :
 
