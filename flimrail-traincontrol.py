668c354429a1cd244c75fddd7de25e1f62100184
Phil B <philb@seepies.net>
2016-02-23 21:06:54 -0500

    Got basic handling of incoming data and rough base function layout ready to interface with the train system

--- flimrail-traincontrol.py
+++ flimrail-traincontrol.py
@@ -4,7 +4,6 @@
 ### to train system
 
 import socket
-import binascii
 
 UDP_LISTEN_IP = "209.141.43.9"
 UDP_LISTEN_PORT = 25605
@@ -15,23 +14,37 @@ sock.bind((UDP_LISTEN_IP, UDP_LISTEN_PORT))
 def get_state() :
     return False 
 
+def control_switcher(command) :
+    cs = {
+        '01': control_go,
+        '03': control_reverse,
+    }
+    m = cs.get(command, control_null)
+    return m()
+
+def control_null() :
+    return 'Nothing to do'
+
+def control_go() :
+    return 'Train is now on'
+
+def control_reverse() :
+    return 'Train is on and in reverse'
+
 def main() :
  
-    try :
-            
-        state = 0
+    state = 0
 
-        do_read = get_state()
+    do_read = get_state()
 
-        while True:
-            f.seek(0)
-            data, addr = sock.recvfrom(1024)
+    while True:
+        data, addr = sock.recvfrom(1024)
+        
+        dr = repr(data)
+        dr = dr[3:len(dr)-1]
 
-            print "Data Length: ", len(data) 
-            print "Data-repr: ", repr(data)
-            print "Data-stripped: ", data.strip()
-            print "Data: ", binascii.b2a_uu(binascii.a2b_uu(data))
-
+        ### print "Data: ", dr 
+        print control_switcher(dr)
 
 if __name__ == '__main__' :
     try :
